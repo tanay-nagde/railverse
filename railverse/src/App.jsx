9c55100fa7560eec238391a60bd7b2bebe71d3f7
tanay-nagde <tanaynagde@gmail.com>
2024-04-13 03:38:47 +0530

    add bg image

--- railverse/src/App.jsx
+++ railverse/src/App.jsx
@@ -4,6 +4,7 @@ function App() {
 
   return (
     <>
+     <div className=" bg-black h-screen w-screen  bg-cover bg-[url('./assets/images/pexels-jakub-pabis-15969781.jpg')] "></div>
      
     </>
   )
